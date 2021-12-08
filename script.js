58c634568332d655e23878093aa10cba2a23da45
smalancea <82141196+smalancea@users.noreply.github.com>
2021-12-08 14:51:57 +0200

    Edit the CSS on mobile version

---
+++ script.js
@@ -0,0 +1,18 @@
+// const form = document.querySelector('form');
+// const iconError = document.getElementById('icon-error');
+// const inputField = document.querySelector('input');
+// const errorMessage = document.getElementById('.error-message');
+
+// const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   const firstName = form['firstname'].value;
+//   const lastName = form['lastname'].value;
+//   const email = form['email'];
+//   const password = form['password'].value;
+
+//   if (inputField === '') {
+//   }
+// });
